b1da07eccfe7b7e314f1ae03c63ceaedf5ed24e3
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-16 10:35:19 -0300

    🎨
    add body,  style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import { Login } from './components/form/login'
+import { Social } from './components/LoginSocial/Social'
+import { Break } from './components/common/break'
 function App() {
   return (
-    <main>
-      <Login />
-    </main>
+    <>
+      <div
+        className='
+      md:justify-center
+
+      w-full h-screen flex flex-col  items-center z-10 relative'
+      >
+        <Login />
+        <footer className='w-full absolute bottom-0'>
+          <Break>if you log with</Break>
+          <Social />
+        </footer>
+      </div>
+
+      <video
+        muted
+        autoPlay
+        loop
+        src='src/assets/rocketseatRSXP[descktop].mp4'
+        className='w-full h-screen object-cover absolute top-0 z-0'
+      ></video>
+    </>
   )
 }
 
