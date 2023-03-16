e93424b14b9643a0e90dd09fa5bb4aa87a5507b8
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-03-16 10:49:16 -0300

    🎨

--- src/components/LoginSocial/Social.tsx
+++ src/components/LoginSocial/Social.tsx
@@ -2,12 +2,12 @@ export const Social = () => {
   return (
     <div className='flex justify-center'>
       <img
-        src='src/assets/svg/icons8-github (1).svg'
+        src='public/icons8-github (1).svg'
         alt='icon for login in githgub'
         className='w-10'
       />
       <img
-        src='src/assets/svg/icons8-google.svg'
+        src='public/icons8-google.svg'
         alt='icon for login in google'
         className='w-10 p-1.5'
       />
