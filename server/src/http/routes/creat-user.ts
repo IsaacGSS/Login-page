f07d3250d3b2c3c76f2595c5d3cf1f1758ef3dd7
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-03-16 03:51:17 -0300

    Update creat-user.ts

--- server/src/http/routes/creat-user.ts
+++ server/src/http/routes/creat-user.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from 'fastify'
-import { fastifyBcrypt } from 'fastify-bcrypt'
 import { prisma } from '../../lib/prisma'
 import z from 'zod'
+import { createSigner, createVerifier } from 'fast-jwt'
 
 export const creatUser = async (app: FastifyInstance) => {
   app.post('/user', async (request, reply) => {
@@ -21,19 +21,18 @@ export const creatUser = async (app: FastifyInstance) => {
 
     if (emailValidate?.email === email) {
       return reply
-        .status(201)
+        .status(400)
         .send({ mensagem: 'O email ja existe, tente outro' })
     }
 
     const hash: any = await app.bcrypt
       .hash(password)
       .then(hash => {
-        console.log('Hash gerado:', hash)
         return hash
       })
       .catch(err => console.error(err.message))
 
-    await prisma.user.create({
+    const newuser = await prisma.user.create({
       data: {
         name,
         email,
@@ -41,6 +40,19 @@ export const creatUser = async (app: FastifyInstance) => {
       }
     })
 
-    return reply.status(201).send({ password: hash })
+    const { password: _, ...user } = newuser
+
+    const signSync = await createSigner({
+      key: process.env.SECRET_KEY_JWT,
+      expiresIn: '7d',
+      header: { alg: 'HS256', typ: 'JWT' }
+    })
+
+    let token = await signSync({ id: user.id })
+
+    const verifySync = createVerifier({ key: process.env.SECRET_KEY_JWT })
+    const payload = verifySync(token)
+
+    return reply.status(201).send({ payload })
   })
 }
